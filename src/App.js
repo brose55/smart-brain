fc13d7a773005308eb0b64b7eba767e1d1500774
Brandon Rose <[brandonrose805@gmail.com]>
2018-08-03 18:21:29 -0700

    set up basic structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
-
+import Navigation from './components/Navigation';
+import Logo from './components/Logo.js';
+import Rank from './components/Rank.js';
+import ImageLinkForm from './components/ImageLinkForm';
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 60,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        New App
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        <header>
+          <Navigation />
+          <Logo />
+        </header>
+        <Rank />
+        <ImageLinkForm />
+        {/* <FacialRecogniton /> */}
       </div>
     );
   }
