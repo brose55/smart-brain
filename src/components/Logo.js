b4c2c5bd2fe7de165d5cc814263a508d8d05ca42
Brandon Rose <brandonrose805@gmail.com>
2020-11-18 21:34:55 -0800

    trying to fix errors

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Tilt from 'react-tilt';
-import brain from '../style/brain2.png';
+import logo from '../style/logo.png';
 import '../style/Logo.css';
 
 const Logo = () => {
@@ -8,7 +8,7 @@ const Logo = () => {
     <div className='ma4'>
       <Tilt className="Tilt br2 shadow-2 logo" options={{ max : 55}}>
         <div className="Tilt-inner pa3">
-          <img src={brain} alt='logo' />
+          <img src={logo} alt='logo' />
         </div>
       </Tilt>
     </div>
